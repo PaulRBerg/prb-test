35f187fbd33425cdd7ca6f1a58965d1d4e7f473f
Paul Razvan Berg <hello@paulrberg.com>
2022-07-16 13:25:44 +0300

    fix: import global symbols from Helpers.sol
    A bug in v0.8.12 makes it not possible to import overloaded free functions
    See discussion in https://github.com/paulrberg/prb-test/issues/5

--- src/PRBTest.sol
+++ src/PRBTest.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
+import "./Helpers.sol";
 import { Vm } from "./Vm.sol";
-
-import { abs, contains, delta, eq } from "./Helpers.sol";
 
 /*//////////////////////////////////////////////////////////////////////////
                                     PRBTEST
--- test/assert-not-eq/assertNotEq.t.sol
+++ test/assert-not-eq/assertNotEq.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.0;
 
-import { eq } from "../../src/Helpers.sol";
+import "../../src/Helpers.sol";
 import { PRBTestTest } from "../PRBTestTest.t.sol";
 
 contract PRBTestTest__AssertNotEq is PRBTestTest {
